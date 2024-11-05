aee8a24069e96c90623d47b04bbefabf769d2d97
mahmouddeshoo <mahmouddshoo@gmail.com>
2024-11-06 02:11:53 +0300

    Js card

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let h2 = "Empower your learning journey online";
+let p = "Babylon - Svelte Education Template is a modern eLearning platform template built with SvelteKit and Tailwind CSS";
+
+
+let card =`  <section class="container">
+        <div id="title">
+        <h2>${h2}</h2>
+        <p>${p}</p>
+        </div>
+        <div id="card">
+            <img src="img/sound.jpg" alt="sound">
+        </div>
+    </section>`;
+document.write(card);
